5fd9834f192ee10e85f88a75055bc62d25755af4
veesds <vssimoes24@gmail.com>
2022-07-12 13:00:56 -0300

    First Commit

---
+++ Downloads/appVit/app/src/main/java/com/example/appvit/Premiado.java
@@ -0,0 +1,14 @@
+package com.example.appvit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Premiado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_premiado);
+    }
+}
---
+++ Downloads/appVit/app/src/main/java/com/example/appvit/balao1.java
@@ -0,0 +1,14 @@
+package com.example.appvit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class balao1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_balao1);
+    }
+}
